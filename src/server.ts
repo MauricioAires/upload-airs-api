744418ca7b5e85dd6e602665802e5fc2429913a4
MauricioAires <mauricioaires015@gmail.com>
2023-09-16 15:13:45 -0300

    feat(generate completion) create docs and method

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import { fastify } from "fastify";
 import { getAllPromptsRoutes } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ia-completion";
 
 const app = fastify();
 
 app.register(getAllPromptsRoutes);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
+app.register(generateAICompletionRoute);
 
 app
   .listen({

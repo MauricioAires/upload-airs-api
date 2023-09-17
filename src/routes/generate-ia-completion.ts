8b75e94430fe2e628c0d0f9881fbb001bc076450
MauricioAires <mauricioaires015@gmail.com>
2023-09-16 21:23:39 -0300

    refactor(generate completion) change params name

--- src/routes/generate-ia-completion.ts
+++ src/routes/generate-ia-completion.ts
@@ -9,11 +9,11 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
   app.post("/ai/complete", async (req, reply) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
     });
 
-    const { template, videoId, temperature } = bodySchema.parse(req.body);
+    const { prompt, videoId, temperature } = bodySchema.parse(req.body);
 
     const video = await prisma.video.findFirstOrThrow({
       where: {
@@ -27,7 +27,7 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
       });
     }
 
-    const promptMessage = template.replace(
+    const promptMessage = prompt.replace(
       "{transcription}",
       video.transcription,
     );

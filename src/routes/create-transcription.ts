63201229e889670acd60230a00d5eefb61857099
MauricioAires <mauricioaires015@gmail.com>
2023-09-20 11:51:32 -0300

    feat(cerate transcription) remove video hast'n audio

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -3,9 +3,10 @@ import { createReadStream } from "node:fs";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
+import fs from "node:fs";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
-  app.post("/videos/:videoId/transcription", async (req) => {
+  app.post("/videos/:videoId/transcription", async (req, reply) => {
     const paramsSchema = z.object({
       videoId: z.string().uuid(),
     });
@@ -37,6 +38,25 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
     });
 
     const transcription = response.text;
+
+    if (!response.text) {
+      fs.unlink(videoPath, function (err) {
+        if (err) return console.log(err);
+        console.log("file deleted successfully");
+      });
+
+      // Remover registro do video sem texto
+      await prisma.video.delete({
+        where: {
+          id: videoId,
+        },
+      });
+
+      return reply.status(400).send({
+        error:
+          "O sistema não foi capaz de identificar nenhum texto no vídeo. Este é um vídeo de música, que geralmente não possui texto. Por favor, selecione outro vídeo que possua áudio",
+      });
+    }
 
     await prisma.video.update({
       where: {
